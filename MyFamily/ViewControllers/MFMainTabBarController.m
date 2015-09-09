7573638eecf8dd0f039358a29a5d9820164ecee8
bok <lbo99@linbok.com>
2015-09-09 19:53:25 +0800

    1.0.0.013-add bar image

--- MyFamily/ViewControllers/MFMainTabBarController.m
+++ MyFamily/ViewControllers/MFMainTabBarController.m
@@ -17,6 +17,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    NSArray *imageArr = [NSArray arrayWithObjects:@"bar_message_gray_30",@"bar_contact_gray_30",@"bar_task_gray_30",@"bar_notif_gray_30",@"bar_more_gray_30", nil];
+    NSArray *imageArr1 = [NSArray arrayWithObjects:@"bar_message_30",@"bar_contact_30",@"bar_task_30",@"bar_notif_30",@"bar_more_30", nil];
+    for (int i = 0; i < [self.tabBar.items count]; i++) {
+        UITabBarItem *item = [self.tabBar.items objectAtIndex:i];
+        
+        NSString *imgStr = [imageArr objectAtIndex:i];
+        item.image = [[UIImage imageNamed:imgStr] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+        
+        NSString *imgStr1 = [imageArr1 objectAtIndex:i];
+        item.selectedImage = [[UIImage imageNamed:imgStr1] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
--- MyFamily/ViewControllers/TabBarPages/MFTaskListController.m
+++ MyFamily/ViewControllers/TabBarPages/MFTaskListController.m
@@ -19,9 +19,14 @@
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated
+{
+    
+    [super viewWillAppear:animated];
+    
+    self.tabBarController.title = @"任务";
+    self.tabBarController.navigationItem.rightBarButtonItem = nil;
+    
 }
 
 /*
